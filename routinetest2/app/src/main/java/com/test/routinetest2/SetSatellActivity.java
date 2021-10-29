157962d4b77f45b5f2fc82af8efa67acc5ec8bb4
seungchan bae <bea0266@naver.com>
2021-10-30 02:25:27 +0900

    위성추가 화면구현, 데이터 생성 구현 예정

--- routinetest2/app/src/main/java/com/test/routinetest2/SetSatellActivity.java
+++ routinetest2/app/src/main/java/com/test/routinetest2/SetSatellActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -22,6 +23,7 @@ public class SetSatellActivity extends Activity {
 
     final Calendar cal = Calendar.getInstance();
 
+    EditText etStartDate, etStartTime, etEndDate, etEndTime;
     TextView habitTitle;
     TextView startDateTime, endDateTime;
     ImageButton btnStartDate, btnStartTime, btnEndDate, btnEndTime;
@@ -31,6 +33,7 @@ public class SetSatellActivity extends Activity {
     Calendar calendar = Calendar.getInstance();
     TextView habitContents;
     int sw[] = {0,0,0,0,0,0,0};
+    int isClickedStartdate = 0, isClickedEnddate = 0, isClickedStarttime = 0, isClickedEndtime=0;
     DatePickerDialog.OnDateSetListener myDatePicker = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
@@ -38,6 +41,11 @@ public class SetSatellActivity extends Activity {
             dYear = String.valueOf(year);
             dMonth = String.valueOf(month+1);
             dDay = String.valueOf(dayOfMonth);
+
+            if (isClickedStartdate==1){
+                etStartDate.setText(dYear+"-"+ dMonth +"-"+ dDay);
+            } else
+                etEndDate.setText(dYear+"-"+ dMonth +"-"+ dDay);
 
         }
     };
@@ -47,6 +55,15 @@ public class SetSatellActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.satell_setting);
+        etStartDate = (EditText) findViewById(R.id.etStartDate);
+        etEndDate = (EditText) findViewById(R.id.etEndDate);
+        etStartTime = (EditText) findViewById(R.id.etStartTime);
+        etEndTime = (EditText) findViewById(R.id.etEndTime);
+
+        btnStartDate = (ImageButton) findViewById (R.id.callendarStart);
+        btnEndDate = (ImageButton) findViewById (R.id.callendarEnd);
+        btnStartTime = (ImageButton) findViewById(R.id.clockStart);
+        btnEndTime = (ImageButton) findViewById(R.id.clockEnd);
 
         for (int i = 0; i < btnWeekIDs.length; i++) {
 
@@ -77,22 +94,28 @@ public class SetSatellActivity extends Activity {
 
 
 
-        btnStartDate = (ImageButton) findViewById (R.id.callendarStart);
-        btnEndDate = (ImageButton) findViewById (R.id.callendarEnd);
-        btnStartTime = (ImageButton) findViewById(R.id.clockStart);
-        btnEndTime = (ImageButton) findViewById(R.id.clockEnd);
+
 
         btnStartDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new DatePickerDialog(SetSatellActivity.this, myDatePicker, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+                isClickedStartdate = 1;
+                isClickedEnddate = 0;
+                new DatePickerDialog(SetSatellActivity.this, myDatePicker,
+                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+
+
             }
         });
 
         btnEndDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new DatePickerDialog(SetSatellActivity.this, myDatePicker, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+                isClickedEnddate = 1;
+                isClickedStartdate = 0;
+                new DatePickerDialog(SetSatellActivity.this, myDatePicker,
+                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+
             }
         });
 
@@ -103,8 +126,16 @@ public class SetSatellActivity extends Activity {
                 TimePickerDialog dialog = new TimePickerDialog(SetSatellActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int hour, int min) {
+                        String msg;
+                        if(hour<12){
+                            msg = String.format("오전 %d시 %d분", hour, min);
+                            etStartTime.setText(msg);
+                        } else {
 
-                        String msg = String.format("%d 시 %d 분", hour, min);
+                            msg = String.format("오후 %d시 %d분", hour-12, min);
+                            etStartTime.setText(msg);
+                        }
+
                         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
                     }
                 }, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false);  //마지막 boolean 값은 시간을 24시간으로 보일지 아닐지
@@ -121,8 +152,15 @@ public class SetSatellActivity extends Activity {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int hour, int min) {
 
-                        String msg = String.format("%d 시 %d 분", hour, min);
-                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                        String msg;
+                        if(hour<12){
+                            msg = String.format("오전 %d시 %d분", hour, min);
+                            etEndTime.setText(msg);
+                        } else {
+
+                            msg = String.format("오후 %d시 %d분", hour-12, min);
+                            etEndTime.setText(msg);
+                        }
                     }
                 }, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false);  //마지막 boolean 값은 시간을 24시간으로 보일지 아닐지
 
